b63df81d1cca6ffe356faef6ce38656e3fb3cdeb
icet-sharadha <icet.sharadha@gmail.com>
2025-03-23 10:45:42 +0530

    commit

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Hello");
+
+let systemNumber=Math.floor((Math.random()*10)+1);
+
+console.log(systemNumber);
+
+alert("Hello");
+
+
+// if(){
+
+// }else if(){
+
+// }
+// else{
+
+// }
+
+
+// switch(4){
+//     case 1:console.log("01");
+//     break;
+//     case 2:console.log("02");
+//     break;
+//     case 3:console.log("03");
+//     break;
+//     case 4:console.log("04");
+//     break;
+// }
